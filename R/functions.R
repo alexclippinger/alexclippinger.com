272caef083335a4843a190b0ab512385a701a3ed
Alex Clippinger <Alex@RDNIEHAUS.COM>
2022-10-11 11:01:01 -0700

    updated project text and links

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,22 @@
 # credit: https://github.com/Ijeamakaanyene/ijeamaka-anyene/blob/master/R/functions.R
 
+create_external_proj_card = function(img_src, url, button, text, text2, text3){
+  tags$div(
+    class = "card",
+    tags$img(
+      class = "card-img-top", 
+      src = img_src),
+    tags$div(
+      class = "card-body",
+      p(text2),
+      h3(text3),
+      h4(text),
+      tags$button(a(href = url, target = "_blank", button))
+      #a(href = url, tags$button(button))
+    )
+  )
+}
+
 create_proj_card = function(img_src, url, button, text, text2, text3){
   tags$div(
     class = "card",
@@ -11,7 +28,7 @@ create_proj_card = function(img_src, url, button, text, text2, text3){
       p(text2),
       h3(text3),
       h4(text),
-      tags$button(a(href = url,  button))
+      tags$button(a(href = url, button))
       #a(href = url, tags$button(button))
     )
   )
